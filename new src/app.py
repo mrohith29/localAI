72dd23311f7056eae5923682095b820c79f154fa
mrohith29 <mariyalarohith29@gmail.com>
2024-04-11 10:24:30 +0530

    new model EleutherAI/gpt-neo-125M

--- new src/app.py
+++ new src/app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 app.config['ENV'] = 'production'
 
 # Load model
-model_name = "EleutherAI/gpt-neo-1.3B"
+# model_name = "EleutherAI/gpt-neo-1.3B"
+model_name = "EleutherAI/gpt-neo-125M"
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 model = GPTNeoForCausalLM.from_pretrained(model_name)
 
@@ -34,28 +35,3 @@ def generate_response():
 if __name__ == '__main__':
     # app.run(debug=True)
      serve(app, host='127.0.0.1', port=5000)
-
-
-
-# from flask import Flask, render_template, request
-# import ollama
-
-# app = Flask(__name__)
-
-# # Home page route
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-# # Route to handle form submission
-# @app.route('/generate', methods=['POST'])
-# def generate_response():
-#     if request.method == 'POST':
-#         question = request.form['question']
-#         # Generate response
-#         message = ollama.chat(model='mistral', messages=[{'role': 'user', 'content': question}])
-#         response = message['message']['content']
-#         return render_template('index.html', question=question, response=response)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
